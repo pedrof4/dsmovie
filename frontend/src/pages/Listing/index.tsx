75ed78f645b267a0b16cc8335e8e3358dc3a071d
drowngut <pmcastrofranca09@gmail.com>
2022-01-10 17:23:51 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,30 @@
 import "./styles.css";
 import Pagination from "components/Pagination";
-
+import MovieCard from "components/MovieCard";
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Listing;
